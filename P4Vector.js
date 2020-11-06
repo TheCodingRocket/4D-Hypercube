763c0aea927955552004edd307b1c64bb2d481e9
TheCodingRocket <73431748+TheCodingRocket@users.noreply.github.com>
2020-11-06 10:56:22 +0700

    Update and rename README.md to P4Vector.js

---
+++ P4Vector.js
@@ -0,0 +1,16 @@
+# 4D-Hypercube
+class P4Vector {
+    constructor(x, y, z, w) {
+        this.x = x || 0;
+        this.y = y || 0;
+        this.z = z || 0;
+        this.w = w || 0;
+    }
+
+    mult(f) {
+        this.x *= f;
+        this.y *= f;
+        this.z *= f;
+        this.w *= f;
+    }
+}
